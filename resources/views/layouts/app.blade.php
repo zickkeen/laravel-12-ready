ce7a5f60faf5a060e9487b74e64796c2c9fa28f1
fatur <fatur@jagoanhosting.com>
2025-05-11 01:51:36 +0700

    Fix generate password profile

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -153,6 +153,20 @@
         @if (session('success'))
             toastr.success("{{ session('success') }}");
         @endif
+        @if (session('error'))
+            toastr.error("{{ session('error') }}");
+        @endif
+        @if (session('warning'))
+            toastr.warning("{{ session('warning') }}");
+        @endif
+        @if (session('info'))
+            toastr.info("{{ session('info') }}");
+        @endif
+        @if ($errors->any())
+            @foreach ($errors->all() as $error)
+                toastr.error("{{ $error }}");
+            @endforeach
+        @endif
     </script>
     @stack('scripts')
 </body>
